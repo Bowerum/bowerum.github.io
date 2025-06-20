e461d12644a65a834b9a83103ac117078d05e62f
Балашов Данил <danil.balashov64@gmail.com>
2025-06-21 01:16:12 +0400

    start

---
+++ javascript/images.js
@@ -0,0 +1,18 @@
+const galleryImages = [
+  {path: "images/gallery/image1.jpg",},
+  {path: "images/gallery/image2.jpg",},
+  {path: "images/gallery/image3.jpg",},
+  {path: "images/gallery/image4.jpg",},
+  {path: "images/gallery/image5.jpg",},
+  {path: "images/gallery/image6.jpg",},
+  {path: "images/gallery/image7.jpg",},
+  {path: "images/gallery/image8.jpg",},
+  {path: "images/gallery/image9.jpg",},
+  {path: "images/gallery/image10.jpg",},
+  {path: "images/gallery/image11.jpg",},
+  {path: "images/gallery/image12.jpg",},
+  {path: "images/gallery/image13.jpg",},
+  {path: "images/gallery/image14.jpg",},
+  {path: "images/gallery/image15.jpg",}
+
+];
